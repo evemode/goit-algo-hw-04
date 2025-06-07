0b74bcd12bf5b2ca8f7dcab7cc81389afecaf22b
Evemode <evemod3@gmail.com>
2025-06-07 11:46:24 +0200

    task1 complete

---
+++ task_1/main.py
@@ -0,0 +1,18 @@
+import pathlib
+
+def total_salary(path:str) -> tuple[float, float]:
+    '''returns tuple (total, average) salary'''
+    salary_path = pathlib.Path(path) # getting salary doc
+    
+    with open(salary_path, 'r', encoding="utf-8") as s:
+        name_salary = [line.strip().split(',') for line in s.readlines() if ',' in line] # create list of salaries 
+    
+        try:
+            salary = [float(money[1]) for money in name_salary]  # extract salaries and convert to float
+        except (ValueError, IndexError):
+            raise ValueError(f'Please, {salary_path}. Failed to convert the salary into a number. ') # return error if found 
+        
+        total = sum(salary) 
+        average = total / len(salary)
+        return (total, average)
+print(total_salary('salary.txt'))
